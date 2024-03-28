7439833ed51a8c63327a7d29171683a2426bd2c5
Luis Henrique Jacinto Da Silva <luis.silva@datamex.com.br>
2024-03-27 21:26:35 -0300

    chore: add Recommendation recommended_at

--- src/Recommendation.php
+++ src/Recommendation.php
@@ -6,10 +6,12 @@ class Recommendation {
 
     private Student $recommended;
     private Student $referrer;
+    private \DateTimeImmutable $recommended_at;
 
     public function __construct(Student $recommended, Student $referrer) {
         $this->recommended = $recommended;
         $this->referrer = $referrer;
+        $this->recommended_at = new \DateTimeImmutable();
     }
 
 }

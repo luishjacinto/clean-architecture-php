7161e0b77b84043cd19c68446334de3d8b05f808
Luis Henrique Jacinto Da Silva <luis.silva@datamex.com.br>
2024-03-27 21:20:30 -0300

    chore: install phpunit

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -31,6 +31,18 @@ class ComposerAutoloaderInita1c2fe9f92d803d266d0d29f990704dd
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInita1c2fe9f92d803d266d0d29f990704dd::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
